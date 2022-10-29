52cd2dad71801a7ed9b0444a3ff2af077faaaee8
Marvin <marvin3747@gmail.com>
2022-10-29 20:19:30 +0800

    init

---
+++ SimpleYTDownloader.py
@@ -0,0 +1,42 @@
+import os
+import PySimpleGUI as sg
+from pytube import YouTube
+
+def main():
+
+    sg.theme('DarkTeal9')   # Add a touch of color
+
+    # All the stuff inside your window.
+    layout = [  [sg.Text('Enter the URL of the YouTube video you want to download', tooltip='Youtube Downloader Just Works')],
+                [sg.InputText(key='-URL-')],
+                [sg.Button('Video', expand_x=True, key='-VIDEO-'), sg.Button('Audio', expand_x=True, key='-AUDIO-')],
+                [sg.Text(key='-OUTPUT-')]
+            ]
+
+    # Create the Window
+    window = sg.Window('Simple YouTube Downloader', layout,font='Serif 12', element_justification='center', resizable=True)
+    # Event Loop to process "events" and get the "values" of the inputs
+    while True:
+        event, values = window.read()
+        # donwload Path
+        path = os.path.join(os.path.expanduser('~'), 'Downloads')
+        # if user closes window
+        if values['-URL-']:
+            yt = YouTube(values['-URL-'])
+            newValues = 'Title\t: ' + yt.title + '\n' + 'Views\t: ' + str(yt.views) + '\n' + 'Durations\t: ' + str(round(yt.length/60, 2)) + '\n'
+            window['-OUTPUT-'].update(newValues)
+        if event == sg.WIN_CLOSED:
+            break
+        elif event == '-VIDEO-':
+            ys = yt.streams.get_highest_resolution()
+            ys.download(output_path=path)
+        elif event == '-AUDIO-':
+            ya = yt.streams.filter(only_audio=True)
+            downFile = ya[0].download(output_path=path)
+            base, ext = os.path.splitext(downFile)
+            new_file = base + '.mp3'
+            os.rename(downFile, new_file)
+    window.close()
+
+if __name__ == '__main__':
+    main()
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Simple Youtube Downloader
+
+Youtube Doenloader That Doesn't Sucks
+
+## Next Todo:
+
+- Youtube Playlist Downloader
+- Subtitle Downloader
+- Select Audio Quality to Download
+- Select Video Quality to Download
